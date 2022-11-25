6c2de8d639eca9431c773dae97cf1378b1388fb0
MarioDanilo0111 <nilovinilo1@gmail.com>
2022-11-26 00:13:28 +0100

    adding updates

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,22 +6,36 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-container">
-        <div className="right">
-          <h4>Kan också hitta oss på Sociala Medier</h4>
+        <div className="footer-one">
+          <h4>här kan du också hitta oss</h4>
           <p>
-            Vi sponsrar de vi samarbetar med och de tjänster vi jobbar med, för
-            att få ut deras erbjudanden till dig, på dessa sociala medier
+            Vi sponsrar dem vi samarbetar med och de tjänster de erbjuder
+            stödjer vi också. Deras erbjudanden kan du också hitta på sociala
+            medier
           </p>
+        </div>
 
+        <div className="footer-two">
           <div className="social">
-            <FaFacebook
-              size={30}
-              style={{ color: "#fff", marginRight: "1rem" }}
-            />
-            <FaInstagram
-              size={30}
-              style={{ color: "#fff", marginRight: "1rem" }}
-            />
+            <div className="fb">
+              <FaFacebook
+                size={30}
+                style={{
+                  color: "#fff",
+                }}
+              />
+            </div>
+            <div className="ig">
+              <FaInstagram
+                size={30}
+                style={{
+                  color: "#fff",
+                }}
+              />
+            </div>
+          </div>
+          <div className="footer-date">
+            <h3>® {new Date().getFullYear()} </h3>
           </div>
         </div>
       </div>
